1d3a40da942ba54725c42de1b9532648a71a610c
VMartinezTorres <81658344+VMartinezTorres@users.noreply.github.com>
2022-05-03 19:05:41 -0400

    sgsdhrk

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,9 @@
 // import { gsap } from "gsap";
+import { gsap } from "gsap";
+import { GSDevTools } from "gsap/GSDevTools";
+import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import { MotionPathHelper } from "gsap/MotionPathHelper";
+
+//register all plugins
+gsap.registerPlugin(GSDevTools, MotionPathPlugin, DrawSVGPlugin, MotionPathHelper);
