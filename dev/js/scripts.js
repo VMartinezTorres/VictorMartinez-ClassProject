f0e258cf305eb002652763554c3ed7d9079ceb03
VMartinezTorres <81658344+VMartinezTorres@users.noreply.github.com>
2022-05-03 20:27:50 -0400

    stuff

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -4,8 +4,13 @@ import { GSDevTools } from "gsap/GSDevTools";
 import { MotionPathPlugin } from "gsap/MotionPathPlugin";
 import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
 import { MotionPathHelper } from "gsap/MotionPathHelper";
+import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
 
 //register all plugins
-gsap.registerPlugin(GSDevTools, MotionPathPlugin, DrawSVGPlugin, MotionPathHelper);
+gsap.registerPlugin(GSDevTools, MotionPathPlugin, DrawSVGPlugin, MotionPathHelper, MorphSVGPlugin);
 
 const mainTL = gsap.timeline();
+
+mainTL.from("#triangle", {duration:1, drawSVG: "50% 50%"})
+.to("#triangle", {duration:1, MorphSVG:"#Rectangle"})
+.to("#triangle", {duration:1, MorphSVG:"#Ellipse"});
